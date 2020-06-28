0ff8d99ed59c5189b598678cd0b9fe6e3ab4ff2a
kino-ma <kino.ma.ms@gmail.com>
2020-06-28 22:26:27 +0900

    change behavior of functions and `token` struct

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #define MAXARG 20
 
 enum token_type {
+    TOKEN_HEAD,
     WORD,
     READ_FILE,
     HEREDOC,
@@ -24,25 +25,38 @@ typedef struct _token {
 } token;
 
 
-token *add_token(token *, char *, int, enum token_type);
+token *new_token(char *, int, enum token_type);
+token *add_token(token *, token *);
 token *tokenize(char *);
 void exec_cmd(token *);
 void exec_cmd_pipe(token *prev, token *next);
-void terminate(token *heod);
+void erase(token *);
+void terminate(token *);
 
 
 
-token *add_token(token *cur, char *str, int d, enum token_type type) {
-    cur->word = calloc(d, sizeof(char));
+token *new_token(char *str, int d, enum token_type type) {
+    token *new = malloc(sizeof(token));
+    new->type = type;
 
-    strncpy(cur->word, str, d+1);
-    cur->word[d] = '\0';
+    if (type == TOKEN_HEAD) {
+        return new;
+    }
 
-    cur->next = malloc(sizeof(token));
-    cur->next->prev = cur;
-    cur->type = type;
+    new->word = calloc(d+1, sizeof(char));
 
-    return cur->next;
+    strncpy(new->word, str, d+1);
+    new->word[d] = '\0';
+    new->next = NULL;
+
+    return new;
+}
+
+token *add_token(token *cur, token *next) {
+    cur->next = next;
+    next->prev = cur;
+
+    return next;
 }
 
 token *tokenize(char *str) {
@@ -50,53 +64,53 @@ token *tokenize(char *str) {
         return NULL;
     }
 
+    enum token_type type;
     token *head, *cur;
-    cur = head = malloc(sizeof(token));
+    cur = head = new_token(NULL, 0, TOKEN_HEAD);
 
     char *s = index(str, '\n');
     *s = '\0';
 
-    for (int i, j = i = 0; i < strlen(str); j++) {
-        enum token_type type = WORD;
+    for (int i = 0, j = i; i < strlen(str); j++) {
+        type = WORD;
 
         // skip spaces
-        while (str[i] == ' ' || str[i] == '\0') {
-            i++, j++;
+        if (str[i] == ' ' || str[i] == '\0') {
+            i++;
+            continue;
         }
 
+        j = i + 1;
         switch (str[i]) {
             case '>':
                 type = REDIRECT_OW;
                 if (str[i+1] == '>') {
                     type = REDIRECT_ADD;
-                    j = i + 1;
+                    j += 1;
                 }
                 break;
             case '<':
                 type = READ_FILE;
                 if (str[i+1] == '<') {
                     type = HEREDOC;
-                    j = i + 1;
+                    j += 1;
                     if (str[i+2] == '<') {
                         type = HERESTR;
-                        j = i + 2;
+                        j += 1;
                     }
                 }
                 break;
             case '|':
                 type = PIPE;
-                j = i + 1;
                 break;
             default:
                 type = WORD;
                 while (index("<>| \0", str[j]) == NULL) {
-                    j++;
+                    j += 1;
                 }
                 break;
         }
-
-        cur = add_token(cur, str + i, j - i, type);
-
+        cur = add_token(cur, new_token(str + i, j - i, type));
         i = ++j;
     }
 
@@ -111,12 +125,121 @@ token *get_cmd() {
     // if failed to get input
     if (getline(&input, &n, stdin) == -1) {
         fprintf(stderr, "failed to get input\n");
-        exit(1);
+        return NULL;
     }
 
     token *head = tokenize(input);
 
     return head;
+}
+
+
+
+void exec_cmd(token *head) {
+    char *command, *args[MAXARG];
+    int argc = 0;
+
+    token *start = head;
+    while (start->type == TOKEN_HEAD) {
+        start = start->next;
+    }
+
+    for (token *cur = start; cur != NULL; cur = cur->next) {
+        // normal token
+        if (cur->type == WORD) {
+            args[argc] = cur->word;
+            argc++;
+        }
+
+        // redirect
+        if (cur->type == REDIRECT_OW || cur->type == REDIRECT_ADD) {
+            int opt;
+            opt = O_WRONLY | O_CREAT;
+
+            if (cur->type == REDIRECT_OW) {
+                opt |= O_TRUNC;
+            } else if (cur->type == REDIRECT_ADD) {
+                opt |= O_APPEND;
+            }
+
+            cur = cur->next;
+            char *filename = cur->word;
+            int fd = open(filename, opt, 0644);
+
+            if (fd < 0) {
+                fprintf(stderr, "failed to open file: %s\n", filename);
+                fprintf(stderr, "%s\n", strerror(errno));
+                exit(1);
+            }
+
+            dup2(fd, 1);
+            close(fd);
+
+            continue;
+        }
+
+        // pipe
+        if (cur->type == PIPE) {
+            token *next = cur->next;
+            cur = cur->prev;
+            erase(cur->next);
+            cur->next = NULL;
+            exec_cmd_pipe(head, next);
+        }
+    }
+
+    args[argc] = NULL;
+
+    execvp(args[0], args);
+
+    // if returned
+    fprintf(stderr, "exec %s failed\n", args[0]);
+    perror("exec");
+    exit(1);
+}
+
+
+
+
+
+int main() {
+    pid_t child_pid = 0;
+    int status;
+    token *cmd_head;
+
+    while (1) {
+        fprintf(stderr, "$ ");
+
+        cmd_head = get_cmd();
+
+        if (cmd_head == NULL || cmd_head->next == NULL) {
+            continue;
+        }
+
+        child_pid = fork();
+
+        if (child_pid < 0) {
+            fprintf(stderr, "failed to fork\n");
+            return 1;
+        }
+
+
+        if (child_pid > 0) {
+            // parent
+            waitpid(child_pid, &status, 0);
+
+            // if child exited with signal
+            if (WIFSIGNALED(status)) {
+                printf("command exited with status: %d\n", WTERMSIG(status));
+            }
+
+            terminate(cmd_head);
+        } else {
+            int pgid = setpgid(0, 0);
+            // child
+            exec_cmd(cmd_head);
+        }
+    }
 }
 
 
@@ -143,7 +266,6 @@ void exec_cmd_pipe(token *prev, token *next) {
         dup2(fds[0], 0);
         close(fds[0]);
 
-        // wait child
         waitpid(pid, NULL, 0);
 
         // exec
@@ -151,7 +273,7 @@ void exec_cmd_pipe(token *prev, token *next) {
     }
 
     /* child */
-    /* output to  parent */
+    /* output to parent */
     if (pid == 0) {
         // close pipe (read)
         close(fds[0]);
@@ -167,110 +289,17 @@ void exec_cmd_pipe(token *prev, token *next) {
 
 
 
-void exec_cmd(token *head) {
-    char *command, *args[MAXARG];
-    int argc = 0;
-    for (token *cur = head; cur->next != NULL; cur = cur->next) {
-        // normal token
-        if (cur->type == WORD) {
-            args[argc] = cur->word;
-            argc++;
-        }
-
-        // redirect
-        if (cur->type == REDIRECT_OW || cur->type == REDIRECT_ADD) {
-            int opt;
-            opt = O_WRONLY | O_CREAT;
-
-            if (cur->type == REDIRECT_OW) {
-                opt |= O_TRUNC;
-            } else if (cur->type == REDIRECT_ADD) {
-                opt |= O_APPEND;
-            }
-
-            cur = cur->next;
-            char *filename = cur->word;
-            int fd = open(filename, opt);
-
-            if (fd < 0) {
-                fprintf(stderr, "failed to open file: %s\n", filename);
-                fprintf(stderr, "%s\n", strerror(errno));
-                exit(1);
-            }
-
-            close(1);
-            dup2(fd, 1);
-            close(fd);
-
-            continue;
-        }
-
-        // pipe
-        if (cur->type == PIPE) {
-            token *next = cur->next;
-            cur->next = NULL;
-            exec_cmd_pipe(head, next);
-        }
+void erase(token *t) {
+    if (t->type != TOKEN_HEAD) {
+        free(t->word);
     }
-
-    args[argc] = NULL;
-
-    execvp(args[0], args);
-
-    // if returned
-    fprintf(stderr, "exec %s failed\n", head->word);
-    perror("exec");
+    free(t);
 }
 
 void terminate(token *head) {
     token *cur, *next;
-    for (cur = head; cur->next != NULL; cur = next) {
+    for (cur = head; cur != NULL; cur = next) {
         next = cur->next;
-        free(cur->word);
-        free(cur);
-    }
-}
-
-
-
-int main() {
-    pid_t child_pid = 0;
-    int status;
-    token *cmd_head;
-
-    while (1) {
-        fprintf(stderr, "$ ");
-
-        cmd_head = get_cmd();
-
-        if (cmd_head == NULL) {
-            continue;
-        }
-
-        child_pid = fork();
-
-        if (child_pid < 0) {
-            fprintf(stderr, "failed to fork\n");
-            return 1;
-        }
-
-
-        if (child_pid > 0) {
-            // parent
-            waitpid(child_pid, &status, 0);
-
-            // if child exited with signal
-            if (WIFSIGNALED(status)) {
-                printf("status: %d\n", WTERMSIG(status));
-            }
-
-            terminate(cmd_head);
-        } else {
-            int pgid = setpgid(0, 0);
-            // child
-            exec_cmd(cmd_head);
-
-        }
-
+        erase(cur);
     }
 }
